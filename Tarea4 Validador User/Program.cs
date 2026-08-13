23ed4d725e1b62ab37eac1929b514d7cdf0d4d79
Ale_Pichi17 <henrylopez17spo@gmail.com>
2026-08-12 18:26:39 -0600

    Agregar archivos de proyecto.

---
+++ Tarea4 Validador User/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Tarea4_Validador_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("%%%% Validador de Nombre De Usuario %%%%");
+            Console.WriteLine("Escribe Tu Nombre");
+            string nombre = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(nombre)) 
+            {
+                Console.WriteLine($"El Nombre No Puede Ser Vacio");
+                return;
+            }
+            nombre = nombre.Trim();
+            string nombreguardado = nombre.ToUpper();
+            Console.WriteLine($"Hola {nombre} guardamos tu nombre como: {nombreguardado}");
+
+            int numPrefijo = Math.Min(4, nombreguardado.Length);
+            string Nombremail = nombreguardado.Substring(0, numPrefijo);
+            Nombremail = Nombremail.ToLower();
+            Console.WriteLine($"Tu correo Institucional Es: {Nombremail}@miumg.edu.gt");
+
+            int validarnombre = nombreguardado.Length;
+            bool Validar = (validarnombre >= 4 && validarnombre <=15);
+            Console.WriteLine(Validar
+                ? $"Usuario Aceptado"
+                : $"Usuario No Valido"
+                );
+
+            int longitud = nombre.Length;
+            int calc = longitud % 2;
+            bool LongitudVal = (calc == 0);
+            Console.WriteLine(LongitudVal
+                ? $"Longitud Par"
+                : $"Longitud Impar"
+                );
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Tarea4 Validador User/README.md
@@ -0,0 +1,27 @@
+# Tarea 4 Validador De Nombre De Usuario Y Crear Correo Institucional
+## Descripción
+
+Programa desarrollado en C# que utiliza:
+
+- Variables int, string y bool
+- Impresiones console.WriteLine
+- string.IsNullOrWhiteSpace
+- if
+- .Trim, .ToUpper, .Substring, .ToLower, .Length
+- Math.Min
+- Expresiones lógicas y Ternarios
+- calculo con % 2
+
+## Cómo ejecutar
+
+- Abrir el proyecto en Visual Studio Community.
+- Presionar "F5".
+- Ingresar un nombre
+
+## Capturas
+
+![Captura 1](Capturas/Cap1.png)
+![Captura 2](Capturas/Cap2.png)
+![Captura 3](Capturas/Cap3.png)
+![Captura 4](Capturas/Cap4.png)
+![Captura 5](Capturas/Cap5.png)
